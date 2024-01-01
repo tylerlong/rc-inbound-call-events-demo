9d2775ffac6ed94c036bc2d067c058604437fedf
Tyler Liu <tyler4long@gmail.com>
2024-01-01 13:22:02 -0800

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import RingCentral from '@rc-ex/core';
+import WebSocketExtension from '@rc-ex/ws';
 
 const rc = new RingCentral({
   server: process.env.RINGCENTRAL_SERVER_URL,
@@ -10,7 +11,10 @@ const main = async () => {
   await rc.authorize({
     jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
   });
-  const r = await rc.restapi().account().extension().get();
-  console.log(r);
+  const wsExt = new WebSocketExtension();
+  await rc.installExtension(wsExt);
+  await wsExt.subscribe(['/restapi/v1.0/account/~/telephony/sessions'], (event) => {
+    console.log(JSON.stringify(event, null, 2));
+  });
 };
 main();
