01e0bf4b4921bb3438d3c1012f2f64d6731da9c9
Tyler Liu <tyler4long@gmail.com>
2024-01-01 13:13:20 -0800

    Hello RingCentral

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log(`Hello ${process.env.NAME}!`);
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
+  });
+  const r = await rc.restapi().account().extension().get();
+  console.log(r);
+};
+main();
